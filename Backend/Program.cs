d790ce8aef3ff4c4463f52b18bf3cd1dfd00e579
OShadow <100359738+0xsissN@users.noreply.github.com>
2025-07-31 12:14:39 -0400

    git

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -15,17 +15,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactApp",
         builder => builder
-            .WithOrigins(
-               "http://localhost:5173",
-               "http://127.0.0.1:5173",
-               "http://localhost:5174",
-               "http://127.0.0.1:5174",
-               "http://192.168.0.12:5174",
-               "http://192.168.0.12:5173",
-               "http://localhost:4173",
-               "http://127.0.0.1:4173",
-               "https://tarija.upds.edu.bo"
-            )
+            .WithOrigins()
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials());
